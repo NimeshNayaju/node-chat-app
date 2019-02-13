d3b972375086b7c1e4365025a2f0c0acdeae6a4f
NimeshNayaju <nimeshnayaju@gmail.com>
2019-02-13 11:39:31 -0400

    broadcasting added

--- server/server.js
+++ server/server.js
@@ -17,6 +17,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'admin@admin.com',
+    text: 'Welcome to the chat app!',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'admin@admin.com',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
     console.log('New message: ', message);
     io.emit('newMessage', {
@@ -24,6 +36,13 @@ io.on('connection', (socket) => {
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // broadcasting a message
+    // socket.broadcast.emit('createMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // })
   });
 
   socket.on('disconnect', () => {
